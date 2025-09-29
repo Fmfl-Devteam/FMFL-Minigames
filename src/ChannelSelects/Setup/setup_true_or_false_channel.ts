35f992797bbb80372445faae2f8fba7b4f97c9df
Fmfl_Felix <felix@fmfl-devteam.de>
2025-09-29 09:44:50 +0200

    feat: add starting message to the selected channel

--- src/ChannelSelects/Setup/setup_true_or_false_channel.ts
+++ src/ChannelSelects/Setup/setup_true_or_false_channel.ts
@@ -1,4 +1,7 @@
+import { ComponentType, TextChannel } from 'discord.js'
 import ChannelSelectMenuInteraction from '../../Contents/Classes/ChannelSelectMenuInteraction'
+import Container from '../../Contents/Classes/Container'
+import COLORS from '../../Contents/Constants/COLORS'
 
 export default new ChannelSelectMenuInteraction({
     customId: 'setup_true_or_false_channel',
@@ -19,5 +22,24 @@ export default new ChannelSelectMenuInteraction({
             content: `The true or false game channel has been set to <#${channelId}>.`,
             components: []
         })
+
+        const container = new Container({
+            accent_color: COLORS.fmfl_blue,
+            components: [
+                {
+                    type: ComponentType.TextDisplay,
+                    content:
+                        '## True or False Minigame\nYou can post statements here to start voting!\nUsers can vote by reacting with ✅ for True and ❌ for False.'
+                }
+            ]
+        }).build()
+
+        const channel = interaction.guild.channels.cache.get(channelId) as TextChannel
+
+        const startMessage = await channel.send({
+            components: [container],
+            flags: 'IsComponentsV2'
+        })
+        await startMessage.pin()
     }
 })
